c2ba3f3a98d0c51a1f05563450def2dac176c878
LacerdaLucca <luccasplacerda@gmail.com>
2022-07-13 16:02:14 -0300

    Classe nota fiscal

---
+++ blueSupermarket/src/main/java/model/Empresa.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Empresa {
+    private String nome;
+    private String cnpj;
+    private String endereco;
+
+    public Empresa(String nome, String cnpj, String endereco) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.endereco = endereco;
+    }
+}
