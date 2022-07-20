81e7959d972c99a0fcaebfd95b03b498b578e7e4
LacerdaLucca <luccasplacerda@gmail.com>
2022-07-19 21:45:23 -0300

    retiradas de souts.

--- blueSupermarket/src/main/java/servlet/UsuarioServlet.java
+++ blueSupermarket/src/main/java/servlet/UsuarioServlet.java
@@ -35,17 +35,11 @@ public class UsuarioServlet extends HttpServlet {
         System.out.println(senha);
         String cep = request.getParameter("cep");
         String endereco = request.getParameter("endereco");
-    //    Usuario usuario = new Usuario(nome, cpf);
-   //     usuarioDao.inserirUsuario(usuario);
         try {
             usuarioService.insereUsuario(nome, cpf, senha, cep, endereco);
         }catch(LoginInvalidoException ex){
             response.setContentType("text/html");
             response.getWriter().println(ex.getMessage() + " <a href ='usuarioForm' > Volte a tela interior </a>");
+        }
     }
-   //     System.out.println(nome + " " + cpf + " " + senha + " " + cep + " " + endereco);
-
-    }
-
-
 }
